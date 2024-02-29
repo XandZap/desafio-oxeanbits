7352dd12951ce9f34924b1efc281bf37fb8dbf32
XandZap <alexandreroberto.94@gmail.com>
2024-02-29 15:56:30 -0300

    🎉 iniciar projeto e configurar a chamada da api

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Suspense } from "react";
+import { useQuery } from "react-query";
+import "./App.css";
+import { getTrendingGiphyData } from "./services/getTrendingGiphyData";
 
 function App() {
+  const { status, data: trendingData, error } = useQuery("randomGiphyData", getTrendingGiphyData);
+  if (!trendingData) return <p>erro</p>;
+  const { data } = trendingData;
+  console.log("data :>> ", data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={<p>carregando...</p>}>
+      <div className="App">
+        {data.map((giphy) => (
+          <div
+            key={giphy.id}
+            style={{
+              border: "1px solid red",
+              maxWidth: giphy.images.fixed_width.width,
+              overflow: "hidden",
+            }}
+          >
+            <h2>{giphy.title}</h2>
+            <img src={giphy.images.fixed_width.url} alt={giphy.title} width={giphy.images.fixed_width.width} />
+          </div>
+        ))}
+      </div>
+    </Suspense>
   );
 }
 
 export default App;
+
---
+++ src/services/getTrendingGiphyData.ts
@@ -0,0 +1,14 @@
+import { ITrendingData } from "../@types/trendingData";
+
+const base_url = process.env.REACT_APP_API_BASE_URL + "trending?api_key=" + process.env.REACT_APP_API_GIPHY_KEY + "&limit=25";
+
+export async function getTrendingGiphyData() {
+  if (!base_url) return;
+  const response = await fetch(base_url);
+  if (!response.ok) {
+    throw new Error("Problema ao buscar dados");
+  }
+  const dados: ITrendingData = await response.json();
+  return dados;
+}
+
